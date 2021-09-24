86a66c3604eb37455b63471d5f79b294f168072d
guyshavit31@gmail.com <Ptukv54321!>
2021-09-24 15:51:28 +0300

    First Math exercise

--- LAB1.py
+++ LAB1.py
@@ -5,7 +5,10 @@ tomato = input("Please enter how many tomatoes do you want: ")
 cucumber = input("Please enter how many cucumbers do you want: ")
 cola = input("Please enter how many Coca Cola do you want: ")
 chicken = input("Please enter the weight of chicken you want: ")
-
+tax = 1.17
 print("\nYour order summary is:\n--------------\nTomatoes: " +str(tomato) + "\nCucumbers: " + str(cucumber) + "\nColas: " + str(cola) + "\nChicken: " + str(chicken) + " KG")
 
-summary = (tomato * 3) + (cucumber * 2) + (cola * 5) + (chicken * 20)
+summary = (int(tomato) * 3) + (int(cucumber) * 2) + (int(cola) * 5) + (int(chicken) * 20)
+
+print("\nyou have to pay " + str(summary) + " NIS without TAX\nYou have to pay " + str("%.2f" % (summary * tax)) + " NIS with TAX")
+
