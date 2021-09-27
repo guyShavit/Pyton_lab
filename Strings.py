c75d71d4eb1f9d2b61ba8b4c600ccead0361f9cd
guyshavit31@gmail.com <Ptukv54321!>
2021-09-27 15:02:40 +0300

    First Strings exercise

---
+++ Strings.py
@@ -0,0 +1,20 @@
+# populate the values in the format function inside the p arrays
+print("p1={},p2={},p3={},p4={red}".format(1,1.0,"guy",red="BBFD"))
+# prints true or false if the sub string exists in the entire string
+print("guy" in "guy shavit")
+
+s1 = "123456"
+# prints the value in 0 position
+print(s1[0])
+# prints the last value in the string
+print(s1[-1])
+# prints from position 1 until last value (not include the last value)
+print(s1[1:-1])
+# prints the string backwards
+print(s1[::-1])
+# prints the values in steps of 2
+print(s1[::2])
+
+
+
+
