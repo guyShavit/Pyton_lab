a48e37ce0ca1057207ebad9e067f3fc8bb60a815
guyshavit31@gmail.com <Ptukv54321!>
2021-09-29 21:35:09 +0300

    second Strings exercise

---
+++ Lists_targil1.py
@@ -0,0 +1,7 @@
+'''
+create a list with 4 details about you: name, age, mail and phone number and print all details to the screen.
+Then create another list with 2 IPs, add 3 more IPs, pop the 3rd IP annd print how many IPs do we have and print them.
+'''
+
+list = ["guy shavit", 39, "guy@gmail.com", "0541234567"]
+
